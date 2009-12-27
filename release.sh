1f583bd94d6cc0aeaa0523a27d53d94af9d29b9f
root <root@mond.de>
2009-12-27 07:06:14 +0100

    nicer release script (removed trailing /)

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-AIPATH=/AI/Skirmish/AGAI/0.1/
+AIPATH=AI/Skirmish/AGAI/0.1/
 TMP=`mktemp -d`
 DIR=`pwd`/AGAI.zip
 
 cp AIInfo.lua ${TMP}
-mkdir -p ${TMP}${AIPATH}
-cp *.jar ${TMP}${AIPATH}
+mkdir -p ${TMP}/${AIPATH}
+cp *.jar ${TMP}/${AIPATH}
 cd ${TMP}
 rm -f ${DIR}
 zip -r ${DIR} .
