8896c95a5e928de08fda381cd36694d8e5c97294
abma <spam@abma.de>
2010-01-01 22:37:14 +0100

    changed helper script

--- helper.sh
+++ helper.sh
@@ -17,21 +17,38 @@ function release(){
 	echo AGAI.zip is ready!
 }
 
-function build(){
-SPRINGINTERFACEPATHS="$HOME/.spring /usr/share/games/spring"
-for i in $SPRINGINTERFACEPATHS; do
-	SPRINGINTERFACE=$i/AI/Interfaces/Java/0.1
-	if [ -e $SPRINGINTERFACE/AIInterface.jar ]; then
-		echo found ai interface: $SPRINGINTERFACE
-		break;
+function _c(){
+	echo $@
+	if ! $@; then
+		echo failed
+		exit 
 	fi
-done
-JAVAOTPS="-d classes -classpath ${SPRINGINTERFACE}/AIInterface.jar:${SPRINGINTERFACE}/jlib/vecmath.jar:${SPRINGINTERFACE}/jlib/jna.jar:SkirmishAI.jar:SkirmishAIReal.jar"
-JAVASRC=`find src-ai -type f -name "*.java"`
-JAVASRCLOADER=agai/loader/*.java
-javac ${JAVAOTPS} ${JAVASRC}
-jar cmf manifest.mf SkirmishAIReal.jar -C classes agai
-
 }
 
+function build(){
+	SPRINGINTERFACEPATHS="$HOME/.spring /usr/share/games/spring"
+	for i in $SPRINGINTERFACEPATHS; do
+		SPRINGINTERFACE=$i/AI/Interfaces/Java/0.1
+		if [ -e $SPRINGINTERFACE/AIInterface.jar ]; then
+			echo found ai interface: $SPRINGINTERFACE
+			break;
+		fi
+	done
+	JAVAOTPS="-d classes -classpath ${SPRINGINTERFACE}/AIInterface.jar:${SPRINGINTERFACE}/jlib/vecmath.jar:${SPRINGINTERFACE}/jlib/jna.jar:SkirmishAI.jar"
+	JAVASRC=`find src-ai -type f -name "*.java"`
+	JAVASRCLOADER=`find src-loader -type f -name "*.java"`
+	echo building loader
+	_c javac ${JAVAOTPS} ${JAVASRCLOADER};
+	_c jar cf SkirmishAI.jar -C classes agai/loader
+	_c javac ${JAVAOTPS} ${JAVASRC}
+	_c jar cf UnderlyingAI-src.jar -C classes agai
+}
+
+function clean (){
+	FILES=CMakeFiles/
+	echo rm -rf $FILES
+	rm -rf $FILES
+}
+
+echo $@
 $@
