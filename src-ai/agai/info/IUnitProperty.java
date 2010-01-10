c3efc42a1e75ecbe5b9f2168a515aff5eb499d19
abma <spam@abma.de>
2010-01-10 16:30:00 +0100

    more comments

--- src-ai/agai/info/IUnitProperty.java
+++ src-ai/agai/info/IUnitProperty.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2009 Matthias Ableitner (http://abma.de/)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package agai.info;
 
 import java.util.Comparator;
@@ -38,10 +54,7 @@ abstract class IUnitProperty implements Comparator<IBuildTreeUnit> {
 	}
 
 	/**
-	 * To be called after the list is changed
-	 * 
-	 * @param unit
-	 *            the unit
+	 * To be called after the list is changed.
 	 */
 	protected void update() {
 		for (int i = 0; i < properties.size(); i++) {
